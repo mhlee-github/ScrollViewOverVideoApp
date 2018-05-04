867ccca87d02bdbe16c4bef9acdb04087191a551
mhlee-SVm <myeonghyeon.lee@insertcoin.me>
2018-05-04 11:45:18 +0900

    initial commit

---
+++ Podfile
@@ -0,0 +1,6 @@
+use_frameworks!
+
+target 'ScrollViewOverVideoApp' do
+  pod 'BMPlayer', '~> 1.0.0'
+  pod 'SnapKit', '~> 4.0.0'
+end
--- ScrollViewOverVideoApp/ViewController.swift
+++ ScrollViewOverVideoApp/ViewController.swift
@@ -7,12 +7,66 @@
 //
 
 import UIKit
+import BMPlayer
+import SnapKit
 
 class ViewController: UIViewController {
 
+  
+  var videoPlayer: BMPlayer!
+//  let videoPlayer = UIView()
+  
+  let scrollView = ScrollView()
+  let shadowView = ShadowView()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    let screenSize = UIScreen.main.bounds.size
+    
+    view.backgroundColor = .white
+
+//    BMPlayerConf.allowLog = true
+//    BMPlayerConf.shouldAutoPlay = false
+    
+    videoPlayer = BMPlayer()
+    let videoAsset = BMPlayerResource(url: URL(string: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")!)
+    videoPlayer.setVideo(resource: videoAsset)
+    
+//    videoPlayer.isUserInteractionEnabled = false
+    
+    scrollView.backgroundColor = .clear
+    scrollView.isUserInteractionEnabled = true
+    scrollView.contentSize = .init(width: screenSize.width*2, height: screenSize.height)
+    scrollView.isPagingEnabled = true
+    
+    shadowView.backgroundColor = UIColor(displayP3Red: 0.5, green: 0.5, blue: 0.5, alpha: 0.5)
+    
+    view.addSubview(videoPlayer)
+    view.addSubview(scrollView)
+    scrollView.addSubview(shadowView)
+    
+    view.bringSubview(toFront: scrollView)
+    
+    videoPlayer.snp.makeConstraints { make in
+      make.top.left.bottom.right.equalTo(view)
+    }
+    
+    scrollView.snp.makeConstraints { make in
+      make.top.left.bottom.right.equalTo(view)
+    }
+
+    shadowView.snp.makeConstraints { make in
+      make.left.equalTo(scrollView).offset(screenSize.width)
+      make.top.equalTo(scrollView)
+      make.size.equalTo(screenSize)
+//      make.left.equalTo(scrollView.snp.right)
+////      make.right.equalTo(scrollView.snp.right).multipliedBy(2)
+//      make.width.equalTo(scrollView.snp.width)
+//      make.top.bottom.equalTo(videoPlayer)
+//
+////      make.size.equalTo(screenSize)
+    }
   }
 
   override func didReceiveMemoryWarning() {
@@ -23,3 +77,10 @@ class ViewController: UIViewController {
 
 }
 
+class ScrollView: UIScrollView {
+  
+}
+
+class ShadowView: UIView {
+  
+}
